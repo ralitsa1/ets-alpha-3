eb71a8f04b2050699d9352165c7abeffcd9aba5d
igloosi <jani.kraner@gmail.com>
2019-01-16 15:20:12 +0000

    Remove validation from add trusted account details

--- app/routes.js
+++ app/routes.js
@@ -130,16 +130,17 @@ router.post('/account/:id/add-a-new-authorised-representative/confirmation', fun
   next()
 })
 
-router.post('/account/:id/add-a-new-trusted-account/account-details', function (req, res, next) {
-  var newTrustedAccountId = req.session.data['new-trusted-account']['id'] || ' '
-  var doesItemExist = req.session.data['existing-accounts'].find(o => o.id === newTrustedAccountId) || false
+// router.post('/account/:id/add-a-new-trusted-account/account-details', function (req, res, next) {
+//   var newTrustedAccountId = req.session.data['new-trusted-account']['id'] || ' '
+//   var doesItemExist = req.session.data['existing-accounts'].find(o => o.id === newTrustedAccountId) || false
 
-  if (newTrustedAccountId !== ' ' && doesItemExist) {
-    res.redirect('/account/' + req.params.id + '/add-a-new-trusted-account/check-account-details')
-  } else {
-    res.redirect('/account/' + req.params.id + '/add-a-new-trusted-account/account-details?error=true')
-  }
-})
+//   // if (newTrustedAccountId !== ' ' && doesItemExist) {
+//     res.redirect('/account/' + req.params.id + '/add-a-new-trusted-account/check-account-details')
+//   // } else {
+//   //   res.redirect('/account/' + req.params.id + '/add-a-new-trusted-account/account-details?error=true')
+//   // }
+//   next()
+// })
 
 router.post('/account/:id/add-a-new-trusted-account/confirmation', function (req, res, next) {
   var newTrustedAccountId = req.session.data['new-trusted-account']['id']
